e364031f5a40502347d07de78622d29b2fca35d5
Jeff Blanchard <jeffrey.blanchard@ais.com>
2022-03-27 14:52:09 -0400

    terraform fmt

--- azure_private_endpoints_and_terraform/key_vault.tf
+++ azure_private_endpoints_and_terraform/key_vault.tf
@@ -10,15 +10,15 @@ resource "random_string" "random" {
 }
 
 resource "azurerm_key_vault" "main" {
-  name                           = "kveus2-${random_string.random.result}"
-  location                       = azurerm_resource_group.kv-rg.location
-  resource_group_name            = azurerm_resource_group.kv-rg.name
-  enabled_for_disk_encryption    = true
-  tenant_id                      = data.azurerm_client_config.current.tenant_id
-  enable_rbac_authorization = true
-  soft_delete_retention_days     = 7
-  purge_protection_enabled       = false
-  sku_name                       = "standard"
+  name                        = "kveus2-${random_string.random.result}"
+  location                    = azurerm_resource_group.kv-rg.location
+  resource_group_name         = azurerm_resource_group.kv-rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  enable_rbac_authorization   = true
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  sku_name                    = "standard"
 }
 
 resource "azurerm_private_endpoint" "main" {
